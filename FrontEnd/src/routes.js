4d4ede532f9dc57f808805be9909888dfbf406ae
BossNMT <97659537+NgoThuan147@users.noreply.github.com>
2022-05-13 21:57:41 +0700

    chinh sua nav

--- FrontEnd/src/routes.js
+++ FrontEnd/src/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 // import PermissionData from './middleware/PermissionData'
 import * as AUTH from 'auth/_store/constants'
 const Dashboard = React.lazy(() => import('dashboard/_views'));
+const DanhMuc = React.lazy(() => import('../src/modules/DanhMuc/_views'));
 
 const routes = [
-  { path: '/', exact: true, name: AUTH.MODULE_DASHBOARD , component: Dashboard }, 
+  { path: '/', exact: true, name: AUTH.MODULE_DASHBOARD , component: Dashboard },
+  { path: '/danh-muc', exact: true, name: AUTH.MODULE_DASHBOARD , component: DanhMuc }, 
   
 ]
 

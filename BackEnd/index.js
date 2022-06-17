e25d4ad9972baecfd4e80cc7c9f3c3b096dafdbf
nampv <nampv@niw.com.vn>
2022-06-17 16:16:41 +0700

    change be

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,66 +1,52 @@
-require('dotenv').config();
-const path = require('path')
-const express = require('express');
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const router = require('./router');
-const handlebars = require('express-handlebars');
+const router = require("./router");
 
-
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 // const cookieParser = require('cookie-parser');
 const http = require("http");
 
 var server = http.createServer(app);
 
-var request = require('request');
+var request = require("request");
 
 // config cors
-var cors = require('cors')
-app.use(cors())
+var cors = require("cors");
+app.use(cors());
 
 // Connect MongoDB
-const mongoose = require('mongoose')
-const mongoDB = 'mongodb+srv://quanlybanhang:quanlybanhang@quanlybanhang.ifaxm.mongodb.net/quanlybanhang?retryWrites=true&w=majority'
-mongoose.connect(mongoDB)
-    .then(() => {
-        console.log('Connect Thanh Cong');
-    })
+const mongoose = require("mongoose");
+const mongoDB =
+  "mongodb+srv://quanlybanhang:quanlybanhang@quanlybanhang.vxijvop.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoDB)
+  .then(() => {
+    console.log("Connect Thanh Cong");
+  })
 
-    .catch((err) => {
-        console.log(err);
-    })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //Cấu hình phương thức GET, POST, PUT, PATCH
-app.use(methodOverride('_method'))
-
-//Cấu hình handlebars
-app.engine('hbs', handlebars({
-    extname: '.hbs',
-    helpers: {
-        handleImg(value) {
-            return value.split('public')[1]
-        }
-    },
-}));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+app.use(methodOverride("_method"));
 
 // Xử lý khi đường dẫn là file tĩnh thì sẽ lao vào public
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, "uploads")));
 
 //Lấy dữ liệu từ POST
-app.use(express.urlencoded({
-	extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
-
-//Lấy dữ liệu từ Cookie
-// app.use(cookieParser())
 
 //Router init
 router(app);
-
 
 //Localhost 3000
 app.listen(PORT, console.log(`Localhost: ${PORT}`));
--- BackEnd/util/mongoose.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    listMongoose: function(mongooses) {
-        return mongooses.map(mongoose => mongoose.toObject());
-    },
-
-    oneMongoose: function(mongoose) {
-        return mongoose ? mongoose.toObject() : mongoose;
-    }
-}
